5ce1236218d3da02ed1c5851bb5a6fda183dfd9a
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-09-03 03:47:37 +0300

    most functionality done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+var livesCount =10;
+
 document.addEventListener('DOMContentLoaded', ()=>{
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
@@ -21,19 +23,38 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const word = document.createElement('div');
     word.id = "letters";
     
-    const lives = document.createElement('div');
-    lives.innerHTML = '<h2>You have  <span id="lives"> </span> Lives</h2>';
+    const livesDiv = document.createElement('div');
+    const testLives = document.createElement('h2');
+    testLives.innerHTML = 'You have  <span id="lives"> </span> Lives';
+    livesDiv.appendChild(testLives);
 
     const result = document.createElement('div');
     result.id = "result";
+
+    const playAgain = document.createElement('input');
+    playAgain.type='button';
+    playAgain.id = "play";
+    playAgain.value = "Play Again";
+    playAgain.addEventListener('click', PlayAgain);
+
+    const hint = document.createElement('input');
+    hint.type='button';
+    hint.id = "hint";
+    hint.value = "Hint";
+    hint.addEventListener('click',GetHint);
 
     document.body.appendChild(Header);
     document.body.appendChild(desc);
     document.body.appendChild(alphabetDiv);
     document.body.appendChild(word);
-    document.body.appendChild(lives);
+    document.body.appendChild(livesDiv);
     document.body.appendChild(result);
+    document.body.appendChild(playAgain);
+    document.body.appendChild(hint);
     
+    const livesCounter = document.getElementById('lives');
+    lives.innerText = livesCount;
+
     startGame();
 })
 
@@ -49,13 +70,14 @@ function startGame(){
 }
 
 
-
+var wordLength =0;
+var correct =0;
+var notWin =false;
 function parseWord(word){
-    const wordLength = word.length;
     const result = document.getElementById('result');
     const inputLetters = document.createElement('p');
     const button = document.querySelectorAll('button');
-
+    wordLength = word.length;
     for(let i = 0; i < wordLength; i++){
         const letter = document.createElement('span');
         letter.id = i;
@@ -64,9 +86,11 @@ function parseWord(word){
 
         
     }
+    
     button.forEach((btn)=>{
         btn.addEventListener('click',()=>{
             if(word.includes(btn.innerText)){
+                
                 const indexes = [];
                 for(let i =0; i<word.length;i++){
                     if(word[i]===btn.innerText){
@@ -77,7 +101,23 @@ function parseWord(word){
                     const dst = document.getElementById(index);
                     dst.innerText = btn.innerText;
                     console.log(index);
+                    correct++;
                 });
+                if(correct === wordLength){
+                    result.innerHTML = "<h3>YOU WIN!!</h3>";
+                    disableButtons();
+                }
+            }
+            else{
+                
+                livesCount--;
+                const livesCounter = document.getElementById('lives');
+                livesCounter.innerText = livesCount;
+
+                if(livesCount===0){
+                    result.innerHTML = "<h3>GAME OVER!</h3>";
+                    disableButtons();
+                }
             }
             btn.disabled = true;
         });
@@ -89,3 +129,24 @@ function parseWord(word){
 
 }
 
+function disableButtons(){
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(btn =>{
+        btn.disabled = true;
+    })
+}
+
+function PlayAgain(){
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(btn =>{
+        btn.disabled = false;
+    })
+    livesCount = 10;
+    const result = document.getElementById('result');
+    result.innerHTML = "<h3></h3>";
+    startGame();
+}
+
+function GetHint(){
+
+}
